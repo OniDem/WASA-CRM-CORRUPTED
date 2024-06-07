87e8afb57aaf841e00b28dd7d4d8c4f1b6d195dc
OniDem1 <Spaindecraft17@gmail.com>
2024-06-07 13:31:45 +0700

    *

--- WASA-Mobile/Pages/SellPage.xaml.cs
+++ WASA-Mobile/Pages/SellPage.xaml.cs
@@ -1,9 +1,61 @@
+using CommunityToolkit.Maui.Views;
+using Core.Entity;
+using System.Collections.ObjectModel;
+using WASA_Mobile.Service;
+
 namespace WASA_Mobile.Pages;
 
 public partial class SellPage : ContentPage
 {
-	public SellPage()
-	{
-		InitializeComponent();
-	}
+    private ProductEntity currentProduct = new();
+    private ObservableCollection<ProductEntity> receiptList = new();
+    public SellPage()
+    {
+        InitializeComponent();
+        BindingContext = receiptList;
+    }
+
+    private void BackButton_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushModalAsync(new MainPage());
+    }
+
+    private async void ScanBarcodeButton_Clicked(object sender, EventArgs e)
+    {
+
+        var popup = new CodeReaderPopUpPage();
+        var result = await this.ShowPopupAsync(popup, CancellationToken.None);
+
+        if (result != null)
+            if (result is string)
+                currentProduct.ProductCode = result as string;
+        currentProduct = await ProductService.SearchProduct(currentProduct.ProductCode);
+        BarcodeEntry.Text = currentProduct.ProductCode;
+        NameEntry.Text = currentProduct.ProductName;
+        CountEntry.Text = "1";
+        PriceEntry.Text = currentProduct.Price.ToString();
+        
+    }
+
+    private void AddProductButton_Clicked(object sender, EventArgs e)
+    {
+        currentProduct.Count = Convert.ToInt32(CountEntry.Text);
+        receiptList.Add(currentProduct);
+        receiptListView.ItemsSource = receiptList;
+        
+        CashButton.IsEnabled = true;
+        AcquiringButton.IsEnabled = true;
+        CashButton.Opacity = 1;
+        AcquiringButton.Opacity = 1;
+    }
+
+    private void PayButton_Clicked(object sender, EventArgs e)
+    {
+        
+    }
+
+    private void AcquiringButton_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
--- WASA-Mobile/Service/UserService.cs
+++ WASA-Mobile/Service/UserService.cs
@@ -15,7 +15,7 @@ namespace WASA_Mobile.Service
             {
                 JsonContent content = JsonContent.Create(request);
                 HttpClient httpClient = new();
-                var response = await httpClient.PostAsync("http://212.20.46.249:32775/User/AuthUser", content);
+                var response = await httpClient.PostAsync("http://212.20.46.249:32769/User/AuthUser", content);
                 if (response.StatusCode == HttpStatusCode.OK)
                 {
                     var result = await response.Content.ReadFromJsonAsync<UserEntity>();
