c786de49b6e09a8be95f712ffc52ae5046dae585
OniDem <90817176+OniDem@users.noreply.github.com>
2024-09-29 22:31:31 +0700

    *

--- WASA-Mobile/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/WASA-Mobile.AssemblyInfo.cs
+++ WASA-Mobile/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/WASA-Mobile.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WASA-Mobile")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ee67af8d63a6f9eb35e165c629bedf940a7894d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ac863fe68a60cf1a6b67a16487c38f1c79b42d8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WASA-Mobile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WASA-Mobile")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
